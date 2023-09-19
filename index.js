1ad17633d4b012e6771926afc77fe902fa6a9a13
ultimated1228 <stevenlucasmeyer@gmail.com>
2023-09-19 10:13:20 -0600

    adding major framework to shapes.js and questions.js.  Tests still need to be created.

--- index.js
+++ index.js
@@ -1,50 +1,123 @@
+// const inquirer = require('inquirer');
+// const fs = require('fs');
+// const { Circle, Polygon, Square } = require('./main/lib/shapes')
+// const questions = require('./lib/questions')
+
+
+// const init = () => {
+//     inquirer.createPromptModule(questions)
+//         .then((data) => {
+//             console.log("Creating SVG File...");
+//             switch ('$(data.shape)') {
+//                 case 'Square':
+//                     console.log('Square is being Created...')
+//                     const square = new Square(data.fill, data.stroke, data.strokeWidth, data.text, data.textColor, data.width, data.width)
+//                     fs.writeFile("./output/logo.svg", square.renderSquare(), (err) => {
+//                         if (err) {
+//                             console.error(err);
+//                         } else {
+//                             console.log('Congrats! Square has been created!')
+//                         }
+//                     }
+//                     );
+//                     break;
+//                 case 'Circle':
+//                     console.log('Circle is being Created...')
+//                     const circle = new Circle(data.fill, data.stroke, data.strokeWidth, data.textColor, data.text, data.radius)
+//                     fs.writeFile("./output/logo.svg", circle.renderCircle(), (err) => {
+//                         if (err) {
+//                             console.error(err);
+//                         } else {
+//                             console.log('Congrats! Circle is now created!')
+//                         }
+//                     });
+//                     break;
+//                 case 'Triangle':
+//                     console.log('Triangle is being Created...')
+//                     const triangle = new Polygon(data.fill, data.stroke, data.strokeWidth, data.text, data.radius, data.textColor)
+//                     fs.writeFile("./output/logo.svg", triangle.renderPolygon(), (err) => {
+//                         if (err) {
+//                             console.error(err);
+//                         } else {
+//                             console.log('Congrats! Triangle is now created!')
+//                         }
+//                     });
+//                     break;
+//             }
+//         });
+//     }
+
+// init();
+
 const inquirer = require('inquirer');
 const fs = require('fs');
-const { Circle, Polygon, Square } = require('./main/lib/shapes')
-const questions = require('./lib/questions')
-
+const { Circle, Polygon, Square } = require('./lib/shapes');
+const questions = require('./lib/questions/questions');
 
 const init = () => {
-    inquirer.createPromptModule(questions)
+    inquirer
+        .prompt(questions)
         .then((data) => {
             console.log("Creating SVG File...");
-            switch ('$(data.shape)') {
+            switch (data.shape) {
                 case 'Square':
-                    console.log('Square is being Created...')
-                    const square = new Square(data.fill, data.text, data.textColor)
-                    fs.writeFile("./output/logo.svg", square.renderSquare(), (err) => {
+                    console.log('Square is being Created...');
+                    const square = new Square(
+                        data.fill,
+                        data.stroke,
+                        data.strokeWidth,
+                        data.textColor,
+                        data.text,
+                        data.width
+                    );
+                    fs.writeFile("./output/logo.svg", square.render(), (err) => {
                         if (err) {
                             console.error(err);
                         } else {
-                            console.log('Congrats! Square has been created!')
+                            console.log('Congrats! Square has been created!');
                         }
-                    }
-                    );
+                    });
                     break;
                 case 'Circle':
-                    console.log('Circle is being Created...')
-                    const circle = new Circle(data.fill, data.textColor, data.text)
-                    fs.writeFile("./output/logo.svg", circle.renderCircle(), (err) => {
+                    console.log('Circle is being Created...');
+                    const circle = new Circle(
+                        data.fill,
+                        data.stroke,
+                        data.strokeWidth,
+                        data.textColor,
+                        data.text,
+                        data.radius
+                    );
+                    fs.writeFile("./output/logo.svg", circle.render(), (err) => {
                         if (err) {
                             console.error(err);
                         } else {
-                            console.log('Congrats! Circle is now created!')
+                            console.log('Congrats! Circle is now created!');
                         }
                     });
                     break;
                 case 'Triangle':
-                    console.log('Triangle is being Created...')
-                    const triangle = new Polygon(data.fill, data.text, data.textColor)
-                    fs.writeFile("./output/logo.svg", triangle.renderPolygon(), (err) => {
+                    console.log('Triangle is being Created...');
+                    const triangle = new Polygon(
+                        data.fill,
+                        data.stroke,
+                        data.strokeWidth,
+                        data.textColor,
+                        data.text,
+                        data.radius
+                    );
+                    fs.writeFile("./output/logo.svg", triangle.render(), (err) => {
                         if (err) {
                             console.error(err);
                         } else {
-                            console.log('Congrats! Triangle is now created!')
+                            console.log('Congrats! Triangle is now created!');
                         }
                     });
                     break;
+                default:
+                    console.log('Invalid shape selected.');
             }
         });
-    }
+};
 
 init();
